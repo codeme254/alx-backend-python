6abbd1248bc1e6a39769f35ebf7675415e6d1c23
codeme254 <otwomadennis365@gmail.com>
2023-04-27 14:33:03 +0300

    create function that adds two float numbers and returns the result as a float as well

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+A type annonated function that takes two floats and return their sum as float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Add two float numbers and return their result as a float
+    """
+    return a + b
