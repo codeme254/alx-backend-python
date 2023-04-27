1424fac6a04232acefb64737da0ff9501f489a65
codeme254 <otwomadennis365@gmail.com>
2023-04-27 14:49:37 +0300

    concatenate two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+takes a string str1 and a string str2 as arguments
+Returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+takes a string str1 and a string str2 as arguments
+Returns a concatenated string
+"""
+    return str1 + str2
