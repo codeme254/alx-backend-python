30057ee8959d25835353ee27e0ed1ba9d5885a93
codeme254 <otwomadennis365@gmail.com>
2023-04-28 17:19:34 +0300

    key value pairs complex types

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+a type-annotated function to_kv
+takes a string k and an int OR float v as arguments
+returns a tuple. The first element of the tuple is the string k.
+The second element is the square of the int/float v and
+should be annotated as a float.
+"""
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+a type-annotated function to_kv
+takes a string k and an int OR float v as arguments
+returns a tuple. The first element of the tuple is the string k.
+The second element is the square of the int/float v and
+should be annotated as a float.
+"""
+    return (k, v * v)
