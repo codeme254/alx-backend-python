e4546e59e84cb7a6dd50207f1ccd8b46f4ed27e3
codeme254 <otwomadennis365@gmail.com>
2023-04-28 16:41:21 +0300

    function that takes in a float and returns an integer

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+take a float n as argument and returns the floor of the float
+"""
+
+def floor(n: float) -> int:
+    """
+    takes a float n as argument and returns the floor of the float
+    """
+    import math
+    return math.floor(n)
